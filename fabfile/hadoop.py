7d495f18c2453b724322d4c31a7729f06ad7cdab
Koji Tanaka <kj.tanaka@gmail.com>
2013-11-03 23:00:54 -0500

    test ssh_authorize

--- fabfile/hadoop.py
+++ fabfile/hadoop.py
@@ -6,7 +6,7 @@ from fabric.api import task, run, sudo, put, task, \
         parallel, execute, env
 from cuisine import file_exists, file_write, file_append, \
         text_strip_margin, mode_sudo, file_update, ssh_keygen, \
-        ssh_authorize
+        ssh_authorize, dir_ensure
 
 @task
 def install():
